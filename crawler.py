a65f44d6ed896a1f52453f8599b9a67b1d81da7f
Gulden Maratovna <55193105+zhgulden@users.noreply.github.com>
2020-06-10 08:22:17 +0600

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -6,17 +6,16 @@ from urllib.request import urlopen
 import collections
 from collections import defaultdict
 
-urls_bfs = []   
-
-longest_page = 'https://en.wikipedia.org/wiki/'
-
 def download_page(url):
     page = urlopen(url)
     html_content = page.read()
     html_content = str(html_content)
+    url = url.split('https://en.wikipedia.org/',1)[1].replace("/","-")
     file = open('pages/' + url + '.txt', 'w')
     file.write(html_content)
     file.close()
+
+urls_bfs = []    
                     
 def get_links_bfs(url):
     urls = []
@@ -31,11 +30,11 @@ def get_links_bfs(url):
                     urls.append(link['href'])
     urls = ['https://en.wikipedia.org' + s for s in urls]
     return urls
-
+                  
 def bfs(url):
     queue = deque([(url,0)])
-    depth = 3
-    while queue and len(set(urls_bfs)) < 1:
+    depth = 1
+    while queue and len(set(urls_bfs)) < 10:
         url,depth = queue.popleft()
         if depth < 3:
                 download_page(url)
@@ -46,4 +45,13 @@ def bfs(url):
                     if link not in queue and link not in urls_bfs:
                         queue.append((link, depth + 1))
                         
-bfs('https://en.wikipedia.org/wiki/')
+def print_bfs_urls():
+    with open('unique-urls-bfs.txt', 'w') as f:
+        for item in urls_bfs:
+            f.write("%s\n" %  item)
+    
+def bfs_crawl(url):
+    bfs(url)
+    print_bfs_urls()
+
+bfs_crawl('https://en.wikipedia.org/wiki/Main_Page')
