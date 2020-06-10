c635f597c51b9daf45cfd97783cdcd98e31e7020
Gulden Maratovna <55193105+zhgulden@users.noreply.github.com>
2020-06-10 08:22:57 +0600

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -34,7 +34,7 @@ def get_links_bfs(url):
 def bfs(url):
     queue = deque([(url,0)])
     depth = 1
-    while queue and len(set(urls_bfs)) < 10:
+    while queue and len(set(urls_bfs)) < 1000:
         url,depth = queue.popleft()
         if depth < 3:
                 download_page(url)
